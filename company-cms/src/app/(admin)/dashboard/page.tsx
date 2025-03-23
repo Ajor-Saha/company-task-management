4fca3e71834cf790cae70980fe3840afd841d070
Ajor-Saha <sokarama79@gmail.com>
2025-03-23 23:46:13 +0600

    delete unnece files

--- company-cms/src/app/(admin)/dashboard/page.tsx
+++ company-cms/src/app/(admin)/dashboard/page.tsx
@@ -6,12 +6,13 @@ import { useRouter } from "next/navigation";
 import { env } from "@/config/env";
 import useAuthStore from "@/store/store";
 import { Axios } from "@/config/axios";
+import { darkColor } from "@/constants";
 
 const Dashboard = () => {
   
 
   return (
-    <div className="p-10">
+    <div className="p-10 dark:bg-[#19191]  h-full">
       <h1>Dashboard</h1>
       <Button onClick={() => console.log("clicked")}>Click me</Button>
     </div>
---
+++ company-cms/src/constants/index.ts
@@ -0,0 +1,7 @@
+export const darkColor = {
+    primary: '#191919',
+    secondary: '#222222',
+    tertiary: '#b0b0b0',
+    quaternary: '#dedede',
+    quinary: '#f0f0f0',
+}
